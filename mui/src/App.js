5a546b827d33a698a36978b4bb6f6adfc27108d6
Aikol Tashbolot uulu <imaikol@yahoo.com>
2021-02-01 11:27:22 -0800

    mui

---
+++ mui/src/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { withStyles, makeStyles } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+
+const MyCustom = withStyles({
+  root: {
+    // "& .MuiInput-underline:after": {
+    //   borderBottomColor: "orange"
+    // },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "blue"
+      },
+      "&:hover fieldset": {
+        borderColor: "yellow"
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "green"
+      }
+    }
+  }
+})(TextField);
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  margin: {
+    margin: theme.spacing(1)
+  }
+}));
+
+export default function App() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root} noValidate>
+      <MyCustom
+        className={classes.margin}
+        label="Custom 1"
+        variant="outlined"
+        id="myId"
+      />
+    </div>
+  );
+}
